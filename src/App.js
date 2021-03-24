2bd41bf2423ddc458c611e3079d1d5eaf9ad0e73
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-03-24 13:31:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Content from'./readmore';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Content/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
